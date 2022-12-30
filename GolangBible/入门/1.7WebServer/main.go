bf375cb2d29393d53d1a98f8e38588e08ecee732
minhongbo <minhongbo@minieye.cc>
2022-12-30 17:27:28 +0800

    study: golang圣经-1.7-WebServer

---
+++ GolangBible/入门/1.7WebServer/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var mtx sync.Mutex
+var count int
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = iota
+	blackIndex
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/gif", func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w, r)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mtx.Lock()
+	count++
+	mtx.Unlock()
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mtx.Lock() // mtx.Lock() 如果获取不到锁会一直阻塞等到可以获取到锁为止
+	fmt.Fprintf(w, "Count %d\n", count)
+	mtx.Unlock()
+}
+
+func lissajous(out io.Writer, r *http.Request) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
