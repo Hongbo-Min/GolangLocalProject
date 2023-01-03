96add42aae2b9eb761d19cff2c1a29cab8761bd2
minhongbo <minhongbo@minieye.cc>
2023-01-03 17:43:40 +0800

    study: golang圣经-5.1-函数声明

--- GolangBible/函数/5.1FuncDeclare/main.go
+++ GolangBible/函数/5.1FuncDeclare/main.go
@@ -1,3 +1,15 @@
 package math
 
 func Sin(X float64) float64
+
+/*
+func add(x int, y int) int   {return x + y}
+func sub(x, y int) (z int)   { z = x - y; return}
+func first(x int, _ int) int { return x }
+func zero(int, int) int      { return 0 }
+
+fmt.Printf("%T\n", add)   // "func(int, int) int"
+fmt.Printf("%T\n", sub)   // "func(int, int) int"
+fmt.Printf("%T\n", first) // "func(int, int) int"
+fmt.Printf("%T\n", zero)  // "func(int, int) int"
+*/
