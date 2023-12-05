3ef0e706c3ba1012e36590166bb82971b5f396d9
minhongbo <minhongbo@minieye.cc>
2023-12-05 10:28:20 +0800

    fix: 修复代码错误

--- 7-days-GeeORM/clause/generator_test.go
+++ 7-days-GeeORM/clause/generator_test.go
@@ -1,8 +1,6 @@
 package clause
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 )
 
@@ -11,53 +9,4 @@ func TestGenBindVars(t *testing.T) {
 	if res != "?,?,?,?,?" {
 		t.Fatal("genBindVars failed res : ", res)
 	}
-}
-
-func _insert(values ...interface{}) (string, []interface{}) {
-	// INSERT INTO $tableName ($fields)
-	tableName := values[0]
-	fields := strings.Join(values[1].([]string), ",")
-	return fmt.Sprintf("INSERT INTO %s (%v)", tableName, fields), []interface{}{}
-}
-
-func _values(values ...interface{}) (string, []interface{}) {
-	// VALUES ($v1), ($v2), ...
-	var bindStr string
-	var sql strings.Builder
-	var vars []interface{}
-	sql.WriteString("VALUES ")
-	for i, value := range values {
-		v := value.([]interface{})
-		if bindStr == "" {
-			bindStr = genBindVars(len(v))
-			sql.WriteString(fmt.Sprintf("(%v)", bindStr))
-			if i+1 != len(values) {
-				sql.WriteString(", ")
-			}
-			vars = append(vars, v...)
-		}
-	}
-	return sql.String(), vars
-}
-
-func _select(values ...interface{}) (string, []interface{}) {
-	// SELECT $fields FROM $tableName
-	tableName := values[0]
-	fields := strings.Join(values[1].([]string), ",")
-	return fmt.Sprintf("SELECT %v FROM %s", fields, tableName), []interface{}{}
-}
-
-func _limit(values ...interface{}) (string, []interface{}) {
-	// LIMIT $num
-	return "LIMIT ?", values
-}
-
-func _where(values ...interface{}) (string, []interface{}) {
-	// WHERE $desc
-	desc, vars := values[0], values[1:]
-	return fmt.Sprintf("WHERE %s", desc), vars
-}
-
-func _orderBy(values ...interface{}) (string, []interface{}) {
-	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
 }
