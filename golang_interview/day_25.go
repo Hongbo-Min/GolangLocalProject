4eb1256013df731bc92711c9fa4674c0bbebda84
minhongbo <906171472@qq.com>
2024-03-16 16:30:35 +0800

    study: add file

---
+++ golang_interview/day_25.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func (stu *Student) Speak(think string) (talk string) {
+	if think == "speak" {
+		talk = "speak"
+	} else {
+		talk = "hi"
+	}
+	return
+}
+
+type People interface {
+	Show()
+}
+
+type Student struct{}
+
+func (stu *Student) Show() {
+}
+
+func main() {
+	var s *Student
+	if s == nil {
+		fmt.Println("s is nil")
+	} else {
+		fmt.Println("s is not nil")
+	}
+	var p People = s
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("p is not nil")
+	}
+}
